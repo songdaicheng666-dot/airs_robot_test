0004a62e25854b1390edb75473561cafa601c3c6
songdaicheng <songdaicheng666@gmail.com>
2026-08-20 22:18:05 +0800

    stop tracking robot-sport

--- robot-sport/ros2_workspace_cpp/src/robot_switch_server/include/robot_switch_server/utils/constants.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace robot_switch_server {
-
-// Shared constants across the robot switch server
-constexpr const char* kUnknownAdapter = "Unknown";
-
-}  // namespace robot_switch_server
